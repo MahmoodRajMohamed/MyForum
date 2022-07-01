3ad0f5b4be8204dde57ea20aaa49e457b6e599ce
MahmoodRajMohamed <89293800+MahmoodRajMohamed@users.noreply.github.com>
2022-07-01 16:21:11 +0530

    trail 0.2

--- app.js
+++ app.js
@@ -41,20 +41,20 @@ app.use('/post',postsroute);
 app.use('/user',userRoute);
 app.use('/nest',nestRoute)
 
-app.use((req, res, next) => {
-    console.log(next)
-    const error = new Error("requesting API not found");
-    error.status = 404;
-    next(error);
-});
+// app.use((req, res, next) => {
+//     console.log(next)
+//     const error = new Error("requesting API not found");
+//     error.status = 404;
+//     next(error);
+// });
 
-app.use((error, req, res, next) => {
-    res.status(error.status || 500)
-        .json({
-            error: {
-                message: error.message
-            }
-        })
-});
+// app.use((error, req, res, next) => {
+//     res.status(error.status || 500)
+//         .json({
+//             error: {
+//                 message: error.message
+//             }
+//         })
+// });
 
 module.exports=app
